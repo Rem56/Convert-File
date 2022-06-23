1e0b31c01a8ef0d674df819f1009215cfcb0dc82
Rem56 <106375921+Rem56@users.noreply.github.com>
2022-06-22 20:52:58 -0400

    Add files via upload

---
+++ Convert.js
@@ -0,0 +1,7 @@
+$(document).ready()
+function alertHover(){
+document.getElementById("area").style.backgroundColor="green";
+}
+function reset(){
+    document.getElementById("area").style.backgroundColor="#953674";
+}
